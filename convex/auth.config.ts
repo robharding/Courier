268105c64bf00f62f183f88360a6d5afd3fa40dd
Rob Harding <robbie.harding@hotmail.com>
2024-03-04 10:23:17 -0500

    Setup clerk auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://apt-marlin-48.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,16 +1,26 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
-import { FC } from 'react'
+import { ConvexReactClient } from "convex/react";
+import { FC } from "react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 interface ConvexClientProviderProps {
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-const ConvexClientProvider: FC<ConvexClientProviderProps> = ({children}) => {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>
-}
+const ConvexClientProvider: FC<ConvexClientProviderProps> = ({ children }) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
 
-export default ConvexClientProvider
+export default ConvexClientProvider;
