301106042ef0603707ef06a730f284b8a4baf782
Rob Harding <robbie.harding@hotmail.com>
2024-03-07 09:11:36 -0500

    Basic loading state for files on dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 
 import { api } from "../../convex/_generated/api";
-import { toast } from "sonner";
-import { ConvexError } from "convex/values";
 import UploadButton from "@/components/UploadButton";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
   const { organization } = useOrganization();
@@ -15,7 +13,6 @@ export default function Home() {
 
   const orgId = organization?.id || user?.id || "";
 
-  const createFile = useMutation(api.files.createFile);
   const files = useQuery(api.files.getFiles, {
     orgId,
   });
@@ -24,29 +21,20 @@ export default function Home() {
     <main className="container pt-8">
       <div className="flex justify-between items-center">
         <h1 className="text-4xl font-bold">Your Files</h1>
-        {/* <Button
-          onClick={() => {
-            if (!orgId) return;
-
-            createFile({ name: "Hello, world.", orgId }).catch((error) => {
-              const errorMessage =
-                error instanceof ConvexError
-                  ? error.data
-                  : "Unexpected error occurred";
-              toast.error(errorMessage);
-            });
-          }}
-        >
-          Upload File
-        </Button> */}
         <UploadButton />
       </div>
 
-      {files?.map((file) => (
-        <div key={file._id}>
-          {file.name}- {file.orgId}
+      {files ? (
+        files.map((file) => (
+          <div key={file._id}>
+            {file.name}- {file.orgId}
+          </div>
+        ))
+      ) : (
+        <div className="flex items-center justify-center">
+          <Loader2 className="mt-12 w-8 h-8 animate-spin" />
         </div>
-      ))}
+      )}
       {organization?.name}
     </main>
   );
