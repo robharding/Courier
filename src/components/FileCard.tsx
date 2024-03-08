b400bd223e27d03bb4b48b2d3aa9a10007210046
Rob Harding <robbie.harding@hotmail.com>
2024-03-08 17:22:34 -0500

    Show a file icon in card depending on type

--- src/components/FileCard.tsx
+++ src/components/FileCard.tsx
@@ -9,21 +9,30 @@ import {
 } from "./ui/card";
 import { Doc } from "../../convex/_generated/dataModel";
 import FileCardMenu from "./FileCardMenu";
+import { FileTextIcon, ImageIcon, VideoIcon } from "lucide-react";
 
 interface FileCardProps {
   file: Doc<"files">;
 }
+
+const FileIcon = {
+  image: <ImageIcon className="h-6 w-6" />,
+  document: <FileTextIcon className="h-6 w-6" />,
+  video: <VideoIcon className="h-6 w-6" />,
+} as const;
 
 const FileCard: FC<FileCardProps> = ({ file }) => {
   return (
     <Card className="relative p-2">
       <FileCardMenu file={file} />
       <CardHeader>
-        <CardTitle className="truncate py-2">{file.name}</CardTitle>
-        <CardDescription></CardDescription>
+        <CardTitle className="flex flex-row items-center gap-2 truncate py-2">
+          <div className="flex-shrink-0 h-full flex">{FileIcon[file.type]}</div>
+          {file.name}
+        </CardTitle>
       </CardHeader>
       <CardContent>
-        <p>Card content</p>
+        <p></p>
       </CardContent>
       <CardFooter>
         <p>card footer</p>
